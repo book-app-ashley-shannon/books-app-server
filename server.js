d2c9dbab5ffed52a0e8b4eb4b15fe7520f5a0174
Shannon Tully <scatman.st@gmail.com>
2017-12-19 21:27:28 -0800

    added get from database

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+app.get('/api/v1/books', (req, res) => {
+  client.query('select book_id, title, author, image_url from books;')
+  .then(results => res.send(results.rows))
+  .catch(console.error);
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
